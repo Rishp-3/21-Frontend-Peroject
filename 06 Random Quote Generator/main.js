88b70b8751d47d63c3bd3d93bc0b77ebc4df25b9
Rishabh <174471886+Rishp-3@users.noreply.github.com>
2026-09-21 13:16:52 +0530

    done

--- 06 Random Quote Generator/main.js
+++ 06 Random Quote Generator/main.js
@@ -1,1 +1,12 @@
-
+const quote = document.querySelector(".quote");
+const author = document.querySelector(".author");
+const button = document.querySelector(".button");
+button.addEventListener("click", function () {
+  fetch("https://dummyjson.com/quotes/random")
+    .then((response) => response.json())
+    .then((data) => {
+      quote.innerText = data.quote;
+      author.innerText = data.author;
+    })
+    .catch((quote.innerText = "Error"));
+});
